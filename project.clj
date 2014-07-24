fd4899026b94ec87ff453370ad907e14e28fda7e
Simon Brooke <simon@journeyman.cc>
2014-07-24 22:04:04 +0100

    Upversioned from 0.1.0 to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject mw-ui "0.1.0"
+(defproject mw-ui "0.1.1-SNAPSHOT"
   :description "Web-based user interface for MicroWorld"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [mw-engine "0.1.0"]
-                 [mw-parser "0.1.0"]
+                 [mw-engine "0.1.1-SNAPSHOT"]
+                 [mw-parser "0.1.1-SNAPSHOT"]
                  [lib-noir "0.8.4"]
                  [ring-server "0.3.1"]
                  [selmer "0.6.8"]
