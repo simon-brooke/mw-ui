e4dff424befa076d27f71b5330a3eec8b26282e9
Simon Brooke <simon@journeyman.cc>
2014-07-27 14:16:14 +0100

    Upversioned from 0.1.2 to 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject mw-ui "0.1.2"
+(defproject mw-ui "0.1.3-SNAPSHOT"
   :description "Web-based user interface for MicroWorld"
   :url "http://www.journeyman.cc/microworld"
   :manifest {
              ;; do not reformat this! It is important for the buildall script
              ;; that each of these properties is on a line with nothing else.
-		"build-signature-version" "0.1.2"
+		"build-signature-version" "0.1.3-SNAPSHOT"
 		"build-signature-user" "Simon Brooke"
 		"build-signature-email" "simon@journeyman.cc"
 		"build-signature-timestamp" "2014-07-27 11:55:34+01:00"
              }
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [mw-engine "0.1.2"]
-                 [mw-parser "0.1.2"]
+                 [mw-engine "0.1.3-SNAPSHOT"]
+                 [mw-parser "0.1.3-SNAPSHOT"]
                  [lib-noir "0.8.4"]
                  [ring-server "0.3.1"]
                  [selmer "0.6.8"]
