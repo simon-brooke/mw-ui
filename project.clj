529731e5f187a6b52776abf3526e55b38bf13947
Simon Brooke <simon@journeyman.cc>
2014-07-27 09:42:44 +0100

    Serious work on the buildall script (and project.clj) to capture a build signature in the MANIFEST.MF file.

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
 (defproject mw-ui "0.1.2-SNAPSHOT"
   :description "Web-based user interface for MicroWorld"
   :url "http://www.journeyman.cc/microworld"
+  :manifest {
+             ;; do not reformat this! It is important for the buildall script
+             ;; that each of these properties is on a line with nothing else.
+		"build-signature-version" "unset"
+		"build-signature-user" "unset"
+		"build-signature-email" "unset"
+		"build-signature-timestamp" "unset"
+             }
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [mw-engine "0.1.2-SNAPSHOT"]
                  [mw-parser "0.1.2-SNAPSHOT"]
