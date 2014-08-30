3e342e41200fb1c377a750f4cc4a0465e1a8fc09
simon <simon@journeyman.cc>
2014-08-30 15:03:02 +0100

    Upversioned from 0.1.3 to 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mw-ui "0.1.3"
+(defproject mw-ui "0.1.4-SNAPSHOT"
   :description "Web-based user interface for MicroWorld"
   :url "http://www.journeyman.cc/microworld"
   :manifest {
@@ -9,8 +9,8 @@
              "Implementation-Version" "unset"
              }
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [mw-engine "0.1.3"]
-                 [mw-parser "0.1.3"]
+                 [mw-engine "0.1.4-SNAPSHOT"]
+                 [mw-parser "0.1.4-SNAPSHOT"]
                  [lib-noir "0.8.4"]
                  [ring-server "0.3.1"]
                  [selmer "0.6.8"]
